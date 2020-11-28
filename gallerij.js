53634c3ebc7ce007d130f2b4e489ef61a47b0d55
bogaert <bogaert.david21@gmail.com>
2020-11-28 17:49:44 +0100

    before menu and gallery

---
+++ gallerij.js
@@ -0,0 +1,9 @@
+var show = document.getElementById("nav-links");
+    function showMenu(){
+        show.style.right = "0";       
+    }
+
+    var hide = document.getElementById("nav-links");
+    function hideMenu(){
+        show.style.right = "-200px";       
+    }
