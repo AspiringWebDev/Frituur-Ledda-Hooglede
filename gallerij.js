c8ba9562d55a1408814732b8f1a89ad8a5620bec
bogaert <bogaert.david21@gmail.com>
2020-11-30 09:43:24 +0100

    owl-carousel working

--- gallerij.js
+++ gallerij.js
@@ -8,7 +8,30 @@ var show = document.getElementById("nav-links");
         show.style.right = "-200px";       
     }
 
+    
 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+          "<i class='fa fa-caret-left'></i>",
+          "<i class='fa fa-caret-right'></i>"
+        ],
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 3
+          },
+          1000: {
+            items: 4
+          }
+        }
+      })
 
 
     
